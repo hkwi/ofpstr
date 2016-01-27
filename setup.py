ee62dabf4f2be4c667f096d8052b43f320ffd1f7
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2016-01-27 22:39:17 +0900

    bump up version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except:
 	from distutils.core import setup
 
 setup(name='ofpstr',
-	version='0.1.5',
+	version='0.1.6',
 	description='Openflow stringer library',
 	long_description=open("README.rst").read(),
 	author='Hiroaki Kawai',
