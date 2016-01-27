097c0ff475b481d41ca79b94759ef1bd7b7cebbd
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2016-01-27 22:42:08 +0900

    pypi returned 500, so bump up version again

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except:
 	from distutils.core import setup
 
 setup(name='ofpstr',
-	version='0.1.6',
+	version='0.1.7',
 	description='Openflow stringer library',
 	long_description=open("README.rst").read(),
 	author='Hiroaki Kawai',
