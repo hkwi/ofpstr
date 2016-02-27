c4ad8c3b1768d568690857eda4e1b68d5d03d7cd
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2016-02-27 10:43:42 +0900

    add cli interface

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ except:
 	from distutils.core import setup
 
 setup(name='ofpstr',
-	version='0.1.11',
+	version='0.1.12',
 	description='Openflow stringer library',
 	long_description=open("README.rst").read(),
 	author='Hiroaki Kawai',
 	author_email='hiroaki.kawai@gmail.com',
 	url='https://github.com/hkwi/ofpstr/',
-	packages=['ofpstr']
+	packages=['ofpstr'],
+	entry_points = {
+		"console_scripts": ["ofpstr_ctl=ofpstr.cli:ofctl"],
+	}
 )
